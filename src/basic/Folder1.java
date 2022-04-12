c6bad43a470474dba5f95d198f769d198b487dab
naina-is <islam.naina15@gmail.com>
2022-04-12 13:05:11 -0400

    first commit

---
+++ src/basic/Folder1.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class Folder1 {
+public static void main(String[] args) {
+	System.out.println("Hi folder");
+}
+}
---
+++ src/basic/Folder2.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class Folder2 {
+	public static void main(String[] args) {
+		System.out.println("Hi folder");
+	}
+}
